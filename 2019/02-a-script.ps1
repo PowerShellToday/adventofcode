0613da111cd5ebe9eabdbee17f319ed387274d32
Pär Hultman <script@hultman.cc>
2019-12-02 22:07:00 +0100

    02-a/b

--- 2019/02-a-script.ps1
+++ 2019/02-a-script.ps1
@@ -7,6 +7,19 @@ $Inputs = Get-Content "$here\02-a-input.txt"
 $Inarray = $Inputs -split ','
 $Inarray[1] = 12
 $Inarray[2] = 2
+"Answer A:"
 (restore-Intcode -optocode $Inarray)[0]
 
-
+"Answer B:"
+for ($noun = 0; $noun -lt 100; $noun++) {
+    for ($verb = 0; $verb -lt 100; $verb++) {
+        $Inarray = $Inputs -split ','
+        $Inarray[1] = $noun
+        $Inarray[2] = $verb
+        $result = (restore-Intcode -optocode $Inarray)[0]
+        if ($result -eq 19690720) {
+            100 * $noun + $verb
+            $noun = $verb = 1000
+        }
+    }
+}
