4065bb1d3837d057b094a657135ff4ad4aabde05
Pär Hultman <par@hultman.cc>
2021-12-01 19:55:01 +0100

    2021 Template

---
+++ 2021/template/solve.ps1
@@ -0,0 +1,44 @@
+function Get-Part1 {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [Object[]]$inputData
+    )
+    
+    begin {
+    }
+    
+    process {
+    }
+    
+    end {
+    }
+}
+
+function Get-Part2 {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [Object[]]$inputData
+    )
+    
+    begin {
+    }
+    
+    process {
+    }
+    
+    end {
+    }
+}
+
+
+$data = 'test'
+$data = 'prod'
+
+$inputData = Get-Content -Path $PSScriptRoot/input_$data.txt
+'Part 1:'
+Get-Part1 -inputData $inputData 
+'Part 2:'
+Get-Part2 -inputData $inputData
+
