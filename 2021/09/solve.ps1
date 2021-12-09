2cb081898b64c0b01031295393ddf2693cb9c65e
Pär Hultman <par@hultman.cc>
2021-12-09 16:42:25 +0100

    2021 09 part2

--- 2021/09/solve.ps1
+++ 2021/09/solve.ps1
@@ -1,3 +1,32 @@
+class point {
+    [int]$X
+    [int]$Y
+    point([int]$X, [int]$Y) {
+        $this.X = $X
+        $this.Y = $Y
+    }
+    [point[]]neighbors($maxX, $maxY) {
+        $Return = @()
+        if ($this.X -lt $maxX) {
+            $return += [point]::new($this.X + 1, $this.Y)
+        }
+        if ($this.X -gt 0) {
+            $return += [point]::new($this.X - 1, $this.Y)
+        }
+        if ($this.Y -lt $maxY) {
+            $return += [point]::new($this.X, $this.Y + 1)
+        }
+        if ($this.Y -gt 0) {
+            $return += [point]::new($this.X, $this.Y - 1)
+        }
+        return [point[]]$return
+    }
+    [string]ToString() {
+        Return ("{0},{1}" -f $this.X , $this.Y)
+    }
+
+}
+
 function Get-Part1 {
     [CmdletBinding()]
     param (
@@ -40,19 +69,48 @@ function Get-Part1 {
 
 function Get-Part2 {
     [CmdletBinding()]
+    
     param (
         [Parameter(Mandatory = $true)]
-        [Object[]]$inputData
+        [string[]]$inputData
     )
     
-    begin {
+    $gridMap = foreach ($string in $inputData) {
+        , [string[]][char[]]$string
     }
-    
-    process {
+            
+    $xmax = $gridMap.Length - 1 
+    $ymax = $gridMap[0].Length - 1
+    $result = @{}
+    $Checked = @{}
+
+    function set-point ([point]$Point, $basin) {
+        $cValue = $gridMap[$Point.X][$Point.Y]
+        $Checked[$Point.ToString()] = 1
+        if ($cValue -match '^[0-8]$' ) {
+            $result[$basin]++
+            foreach ($neighbor in $Point.neighbors($xmax, $ymax)) {
+                if (-not $Checked.ContainsKey($neighbor.ToString())) {
+                    set-point -point $neighbor -basin $basin
+                }
+            }
+        }
     }
-    
-    end {
+    foreach ($x in 0..$xmax) {
+        foreach ($y in 0..$ymax) {
+            $cPoint = [point]::new($x, $y)
+            if (-not $Checked.ContainsKey($cPoint.ToString())) {
+                $cValue = $gridMap[$x][$y]
+                $Checked[$cPoint.ToString()] = 1
+                if ($cValue -match '^[0-8]$' ) {
+                    set-point -point $cPoint -basin $cPoint.ToString()
+                }
+            }
+        }
     }
+
+    $v1, $v2, $v3 = ($result.GetEnumerator() | Sort-Object -Property Value -Descending -Top 3).value
+    $v1 * $v2 * $v3
 }
 
 
